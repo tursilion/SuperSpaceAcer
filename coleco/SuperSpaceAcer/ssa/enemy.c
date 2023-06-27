265c757680256fe18e35c275f1a5370ca871c247
Tursi <tacobell@harmlesslion.com>
2023-06-27 04:30:50 -0600

    - implement enemy generators

--- coleco/SuperSpaceAcer/ssa/enemy.c
+++ coleco/SuperSpaceAcer/ssa/enemy.c
@@ -1,78 +1,8 @@
-// Looks like we have lost:
-// - enemy generators
-// - beamgen enemy (except for the definition)
-// - jets diving left or right as they get close
-// my notes from November 2016:
-/*
-So how do the generators work? The game supports 1-3 at a time, depending on the difficulty and the level you're on. 
-Each generator is mostly independent, more on that in a moment. Each generator further tracks the following data: 
-horizontal position, countdown timer, maximum timer, movement speed, count of enemies remaining, and type of enemy 
-to generate. They are always positioned at the top of the screen because, in SSA, all enemies begin life at the top 
-of the screen.
-
-In order to guarantee the RNG, the generators are checked at fixed points in the level progression -- right now every 
-32 frames the next generator is checked. (Although because I do this with bitmasking, and there are only 3 generators, 
-every fourth check does nothing. Also, in levels that don't have all three generators out, those idle generators end 
-up doing nothing as well. This is partly for the difficulty curve and partly to control CPU time.)
-
-If we check and determine that the generator of interest this frame is idle, and it's allowed to start (again, due to 
-level and difficulty), then we set the RNG based (mostly) on the current level distance counter. Then we create the 
-generator randomly, setting up its position, timer, speed, count and type. The position is where enemies will be 
-dispatched (horizontally), the timer is how many frames between enemy dispatches, the speed is how far (and which 
-way) the generator moves every frame, the count is how many enemies will be dispatched before the generator returns
-to idle, and the type is what type of enemy that will be. These last two values are constrained by the level and
-difficulty settings. The timer is also constrained by the enemy type in order to keep a reasonable minimum spacing. 
-In effect, it's a rudimentary particle system generator.
-
-There's one additional catch -- to avoid overlap of enemies from being too common a thing, we check the spacing 
-against the other generators and reset the position if it's too close to one of them.
-
-During the enemy processing frame, generators are updated. The update is very simple - the speed is added to the 
-position (and wraps around natively with no need to check, since we have 256 pixels across), then the timer is 
-counted down. When it reaches zero, the generator calls the enemy spawn function, and decrements its count. When 
-the count reaches zero, the generator goes idle and will be awakened the next time the distance update reaches it.
-
-Enemy spawn is straight-forward now. We know the type of enemy, and we know where it will start. We also know 
-it's legal for the level and don't need to check that anymore either. But there are still two things to check.
-One is whether there is a legal slot to spawn the enemy in -- both the game engine and the difficulty constrain
-this. If a free slot is not available, then the opportunity to spawn is discarded. (This is why on the easier 
-difficulties you will see more enemies if you shoot them than if you don't!) A second check is also performed 
-for overlap - if the generator is too close to one of the other generators, it's deferred rather than discarded, 
-on the assumption that the generators will move apart.
-
-The overlap avoidance code actually created an interesting bug that only occurred in my testing midway through 
-the medium difficulty level (it might still be there, I didn't finish fixing it... but I didn't see it tonight.) 
-Anyway... about halfway through the stage, enemies stopped appearing, and only spawned one enemy every long time 
-right at the edge until the boss came out. I was a little baffled and added debug to show me where the generators
-were. And at the magic point, I watched generators 1 and 2 chase each other across the screen. They had started
-wrapped around (255 and 1) so passed the creation range check, but were moving the same direction at the same 
-speed, and so were always too close to spawn -- except for the one frame where they were split at the edge of 
-the screen again. (Earlier, before I put the test in the generator startup code, I'd seen a similar case where
-two generators started right beside each other with a speed of zero. Never moved, and couldn't spawn, so the
-stage stayed empty.)
-
-Anyway, I'm also proud of the beamgens. SSA uses all the available sprites and the game engine is limited 
-in the number of enemies it allows - 6 enemies, each with their own shot. It was not intended to support a 
-single enemy with two widely spread parts, so I was going to implement it as two enemies. But I was getting 
-really concerned about how to find empty slots and tracking the two indexes, and ultimately I went for a 
-flicker approach where the generator parts alternate each frame. This worked surprisingly well (and you'll 
-see as a result that they still flicker on the F18A, even though no other sprites do ;) ). The beam itself 
-is just the enemy's bullet, so that was a guaranteed spare.
-
-Testing collision on the main sprite was easy, but I had to think a bit about the secondary one.. ultimately, 
-I decided to just try actually moving the enemy back and forth, and seeing if collisions lasted long enough 
-that either could be shot. Happily, the answer was yes, so all I had to do in the enemy death function was 
-check against the base X position to see whether the right or left generator was the one destroyed, and then 
-set up the enemy as a dud that looked like the remaining one. I did something very similar to this in Mario 
-Bros, actually. The bonus stage broke my enemy engine by having too many coins to collide with. Rather than 
-add a bunch of special cases, it flickers just four coins through all 10 visible ones for the sake of the 
-collision engine. It's something like 1 in 3 frames on the worst case, but the slope of Mario's jump guarantees
-contact for that long for all but the most contrived cases. (Of course, each coin you collect improves this as
-well, once you have two coins it's 1 frame in 2, and when there are only four coins left they are testing
-collision every frame).
-
-*/
-
+// TODO: need to tune generators for consistent flow
+// TODO: why do I get more than 3 mines at at time?
+// TODO: can we make the first stage with the beamgen start with just beamgens?
+// TODO: saucers can start too close to the right edge?
+// TODO: Helicopters that just appear near the bottom?
 
 // libti99
 #include <vdp.h>
@@ -91,6 +21,27 @@ const signed char sinemove[32] = {	0,1,2,2,3,3,4,4,
 									-4,-4,-4,-3,-3,-2,-2,-1 
 								 };
 
+const unsigned char mincnt[7] = {
+    5,  // saucer
+    2,  // jet
+    8,  // mine
+    5,  // helicopter
+    24, // beamgen
+    4,  // swirly
+    12  // bomb
+};
+
+// per level (first level is 1, so 0 is a dummy
+// then 8 random numbers (0-7)
+const unsigned char oddstable[6][8] = {
+    {   ENEMY_SAUCER, ENEMY_JET, ENEMY_SAUCER, ENEMY_JET,     ENEMY_SAUCER,     ENEMY_JET,        ENEMY_SAUCER,     ENEMY_JET },
+    {   ENEMY_SAUCER, ENEMY_JET, ENEMY_SAUCER, ENEMY_JET,     ENEMY_SAUCER,     ENEMY_JET,        ENEMY_SAUCER,     ENEMY_JET },
+    {   ENEMY_SAUCER, ENEMY_JET, ENEMY_MINE,   ENEMY_JET,     ENEMY_SAUCER,     ENEMY_JET,        ENEMY_SAUCER,     ENEMY_JET },
+    {   ENEMY_SAUCER, ENEMY_JET, ENEMY_MINE,   ENEMY_JET,     ENEMY_SAUCER,     ENEMY_HELICOPTER, ENEMY_SAUCER,     ENEMY_HELICOPTER },
+    {   ENEMY_SAUCER, ENEMY_JET, ENEMY_MINE,   ENEMY_BEAMGEN, ENEMY_HELICOPTER, ENEMY_JET,        ENEMY_SAUCER,     ENEMY_HELICOPTER },
+    {   ENEMY_SAUCER, ENEMY_JET, ENEMY_MINE,   ENEMY_BEAMGEN, ENEMY_HELICOPTER, ENEMY_SWIRLY,     ENEMY_BOMB,       ENEMY_SAUCER }
+};
+
 uint8 enr[12], enc[12];					// enemy row and col
 unsigned char ent[12];					// enemy type
 uint8 ech[12], eec[12], esc[12];		// enemy animation - character, end character, start character
@@ -99,6 +50,142 @@ char ep[6];								// enemy power (hit points)
 void (*en_func[12])(uint8);				// pointer to enemy handler function
 unsigned char MineTipPos;
 
+// generators
+unsigned char genx[3], gentime[3], genmax[3], gencount[3], gentype[3];
+signed char genspeed[3];
+
+// set up generators for the stage
+extern int distns;
+void initGenerators() {
+    for (unsigned char b=0; b<3; ++b) {
+        gencount[b]=0;
+        genspeed[b]=b;  // make sure these never start identical
+    }
+}
+
+// update and process the generators
+void enout() {
+    unsigned char maxgen = 3;
+    if (nDifficulty != DIFFICULTY_HARD) maxgen=2;
+
+    // check if we can start a generator
+    if ((distns&0xf)==0) {                                                                                         
+        // 16 frames - it is time
+        // update the RND seed before we do any RNG
+        seed = (unsigned char)(level*8+distns/32);
+        for (unsigned char idx=0; idx<maxgen; ++idx) {
+            if (gencount[idx] == 0) {
+                // generators are always active
+			    unsigned char a=(rndnum()&7);
+                // this counts!
+                genx[idx]=rndnum();                     // x pos
+                genmax[idx]=(rndnum()&0x7)+mincnt[a];   // time between enemies
+                gentime[idx]=genmax[idx];               // countdown timer
+                gencount[idx]=(rndnum()&0x07);          // how many
+				gentype[idx] = oddstable[level][a];     // rebase into an enemy type
+                genspeed[idx]=rndnum()&0xbf;            // max of 64 positive or negative, I think
+
+                if (gentype[idx] == ENEMY_MINE) gencount[idx]=(rndnum()&0x03);
+                if (gentype[idx] == ENEMY_BEAMGEN) gencount[idx]=(rndnum()&0x03);
+                if (gentype[idx] == ENEMY_BOMB) gencount[idx]=1;
+                if (gentype[idx] == ENEMY_JET) genspeed[idx]>>=2;
+
+                // only set up one per time
+                break;
+            }
+        }
+    }
+
+    // update any active generators
+    for (unsigned char idx=0; idx<maxgen; ++idx) {
+        if (gencount[idx]) {
+            // TODO - debug info, clear old generator
+            vdpchar(genx[idx]>>3, ' ');
+
+            genx[idx]+=genspeed[idx];
+
+            // TODO - debug info, show the generator onscreen
+            vdpchar(genx[idx]>>3, '1'+idx);
+
+
+            if (0 == --gentime[idx]) {
+                // time to launch an enemy - maybe - make sure we aren't too close to edges or another active generator
+                unsigned char k,c;
+                unsigned char ok=1;
+
+                // if we can't launch, we still count it
+                gentime[idx] = genmax[idx];
+                --gencount[idx];
+
+                if ((genx[idx]<28)||(genx[idx]>227)) {
+                    continue;  // too close to edge
+                }
+                for (unsigned char b=0; b<maxgen; ++b) {
+                    if (b==idx) continue;
+                    if (gencount[b]==0) continue;
+                    if (abs(genx[idx]-genx[b]) < 32) {
+                        // just a safety check to prevent lockstep
+                        if (genspeed[idx]==genspeed[b]) ++genspeed[idx];
+                        ok=0;
+                        break;
+                    }
+                }
+                if (!ok) {
+                    continue;
+                }
+
+                k=255;
+                for (unsigned char b=0; b<nDifficulty; ++b) {
+                    if (b>5) break;
+                    if (ent[b] == ENEMY_NONE) {
+                        k=b;
+                        break;
+                    }
+                }
+                if (k == 255) {
+                    // no free slot
+                    continue;
+                }
+
+                // all right, load it up
+                ent[k]=gentype[idx];
+				enr[k]=1;
+				enc[k]=genx[idx];
+                c=COLOR_WHITE;  // why is this getting through?
+
+                switch (gentype[idx]) {
+				    // minimum player damage is 2 for spread, 3 for pulse
+                    default:    //for (;;) { } // not sure why we need a default, but we seem to. So make it a saucer.
+					case ENEMY_SAUCER:		eec[k]=0; esc[k]=0; ep[k]=2; c=COLOR_MEDGREEN; en_func[k]=enemysaucer; if (enc[k]<32) enc[k]+=32; if (enc[k]>216) enc[k]-=32; break;
+					case ENEMY_JET:			eec[k]=4; esc[k]=4; ep[k]=3; ecs[k]=0; c=COLOR_LTBLUE; en_func[k]=enemyjet; break;
+					case ENEMY_MINE:		eec[k]=8; esc[k]=8; ep[k]=10; c=COLOR_CYAN; en_func[k]=enemymine; break;
+					case ENEMY_HELICOPTER:	eec[k]=24; esc[k]=12; ep[k]=5; c=COLOR_MAGENTA; ers[k]=(rndnum()&0x7f)+1; ecs[k]=(rndnum()&0x07); if (enc[k]>127) ecs[k]=-ecs[k]; en_func[k]=enemyhelicopter; break;
+					case ENEMY_SWIRLY:		eec[k]=40; esc[k]=28; ep[k]=8; c=COLOR_MEDRED; en_func[k]=enemyswirly; break;
+					case ENEMY_BOMB:		eec[k]=44; esc[k]=44; ep[k]=20; c=COLOR_DKYELLOW; en_func[k]=enemybomb; break;
+					case ENEMY_BEAMGEN:		
+						eec[k]=80; esc[k]=76; ep[k]=14; c=COLOR_GRAY; en_func[k]=enemybeamgen; 
+						if (enc[k] > 144) enc[k]-=128;
+						break;
+				} 
+				if (scoremode == 3) c=COLOR_BLACK;	// invisible enemies (TODO: if the background color changes, it may not be black)
+				ech[k]=esc[k];
+				sprite(k+ENEMY_SPRITE,ech[k],c,enr[k],enc[k]);
+            }
+        }
+    }
+
+	/*shoot?*/
+    {
+	    unsigned char k=rndnum()&15;
+	    if (k<nDifficulty) {	// 1, 3 or 7
+		    if ((ent[k+6]==ENEMY_NONE)&&((ent[k]==ENEMY_SAUCER)||(ent[k]==ENEMY_JET))) { 
+			    shootplayer(k, k+6);
+		    }
+	    }
+    }
+}
+
+#if 0
 void enout()
 { /* rndly bring out a new enemy */
 	uint8 k, a;
@@ -134,15 +221,8 @@ void enout()
 			}
 		}
 	}
-
-	/*shoot?*/
-	k=rndnum()&15;
-	if (k<nDifficulty) {	// 1, 3 or 7
-		if ((ent[k+6]==ENEMY_NONE)&&((ent[k]==ENEMY_SAUCER)||(ent[k]==ENEMY_JET))) { 
-			shootplayer(k, k+6);
-		}
-	}
 }
+#endif
 
 void enemysaucer(uint8 x) {
 	enr[x]+=4;
@@ -484,9 +564,12 @@ void enemybeamgen(uint8 x) {
 		noen(x+6);
 	}
 	if (ent[x] != ENEMY_NONE) {
+        unsigned char c = COLOR_GRAY;
+        if (scoremode == 3) c=COLOR_BLACK;
+
 		if (ent[x+6] != ENEMY_BEAM) {
 			// we don't have both sprites available, just draw the one we have
-			sprite(x+ENEMY_SPRITE, ech[x], COLOR_GRAY, enr[x], enc[x]);
+			sprite(x+ENEMY_SPRITE, ech[x], c, enr[x], enc[x]);
 		} else {
 			// we have a 4 frame cycle to flicker the two sprites over 3 positions
 			// the beam alternates every frame, while the generators get 2
@@ -494,16 +577,16 @@ void enemybeamgen(uint8 x) {
 			// we'll just use the row
 			switch (enr[x]&3) {
 				case 0:	// left and beam
-					sprite(x+ENEMY_SPRITE, 76, COLOR_GRAY, enr[x], min);
+					sprite(x+ENEMY_SPRITE, 76, c, enr[x], min);
 					sprite(x+6+ENEMY_SPRITE, ech[x+6], COLOR_LTYELLOW, enr[x+6], enc[x+6]);
 					break;
 				case 3:
 				case 1:	// left and right
-					sprite(x+ENEMY_SPRITE, 76, COLOR_GRAY, enr[x], min);
-					sprite(x+6+ENEMY_SPRITE, 80, COLOR_GRAY, enr[x], max);
+					sprite(x+ENEMY_SPRITE, 76, c, enr[x], min);
+					sprite(x+6+ENEMY_SPRITE, 80, c, enr[x], max);
 					break;
 				case 2:	// right and beam
-					sprite(x+ENEMY_SPRITE, 80, COLOR_GRAY, enr[x], max);
+					sprite(x+ENEMY_SPRITE, 80, c, enr[x], max);
 					sprite(x+6+ENEMY_SPRITE, ech[x+6], COLOR_LTYELLOW, enr[x+6], enc[x+6]);
 					break;
 			}
@@ -647,6 +730,8 @@ void enemyinit() {
 		ent[a]=ENEMY_NONE;
 	}
 	MineTipPos=0;
+
+    initGenerators();
 }
 
 void pwr(uint8 x)
